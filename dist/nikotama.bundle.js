f2ffc70d28362e74dce3eb545d16599864e9abd4
Fumikazu Fujiwara <github@ze.gs>
2016-07-25 08:23:23 +0900

    add jsdocs

--- dist/nikotama.bundle.js
+++ dist/nikotama.bundle.js
@@ -51,20 +51,39 @@
 /* 1 */
 /***/ function(module, exports) {
 
+	/**
+	 * @classdesc This is Nikotama Class
+	 * @constructor
+	 */
 	'use strict'
 	var Nikotama = function () {
 	  this.JSONPCount = 0
 	}
+	/**
+	 * get
+	 * @param url
+	 * @param callback
+	 * @param JSONPCallback
+	 */
 	Nikotama.prototype.get = function (url, callback, JSONPCallback) {
+	  // create <script async="" src="[url]" type="application/javascript"></script>
 	  var script = document.createElement('script')
 	  script.async = true
 	  script.src = this.setJSONPFunction(url, callback, JSONPCallback)
 	  script.type = 'application/javascript'
 	  var head = document.getElementsByTagName('head')[0]
 	  head.appendChild(script)
+	  // prevent to duplicate of callback function name
 	  this.JSONPCount++
 	}
+	/**
+	 * setJSONPFunction
+	 * @param url
+	 * @param callback
+	 * @param JSONPCallback
+	 */
 	Nikotama.prototype.setJSONPFunction = function (url, callback, JSONPCallback) {
+	  // generate temporary functoin for JSONP
 	  var pad = function (n) { return n < 10 ? '0' + n : n }
 	  if (typeof JSONPCallback === 'undefined') {
 	    var d = new Date()
@@ -74,15 +93,24 @@
 	    JSONPCallback += pad(d.getDate())
 	    JSONPCallback += this.JSONPCount
 	  }
+	  // set global function
 	  window[JSONPCallback] = function (data) {
 	    try {
 	      callback(data)
 	    } catch (e) {}
+	    // cleaning
 	    window[JSONPCallback] = undefined
 	  }
+	  // add callback function to end of the url
 	  url += (url.indexOf('?') === -1) ? '?callback=' + JSONPCallback : '&callback=' + JSONPCallback
 	  return url
 	}
+	/**
+	 * on
+	 * @param target
+	 * @param event
+	 * @param callback
+	 */
 	Nikotama.prototype.on = function (target, event, callback) {
 	  if (document.addEventListener) {
 	    target.addEventListener(event, callback, false)
--- src/nikotama.js
+++ src/nikotama.js
@@ -1,17 +1,36 @@
+/**
+ * @classdesc This is Nikotama Class
+ * @constructor
+ */
 'use strict'
 var Nikotama = function () {
   this.JSONPCount = 0
 }
+/**
+ * get
+ * @param url
+ * @param callback
+ * @param JSONPCallback
+ */
 Nikotama.prototype.get = function (url, callback, JSONPCallback) {
+  // create <script async="" src="[url]" type="application/javascript"></script>
   var script = document.createElement('script')
   script.async = true
   script.src = this.setJSONPFunction(url, callback, JSONPCallback)
   script.type = 'application/javascript'
   var head = document.getElementsByTagName('head')[0]
   head.appendChild(script)
+  // prevent to duplicate of callback function name
   this.JSONPCount++
 }
+/**
+ * setJSONPFunction
+ * @param url
+ * @param callback
+ * @param JSONPCallback
+ */
 Nikotama.prototype.setJSONPFunction = function (url, callback, JSONPCallback) {
+  // generate temporary functoin for JSONP
   var pad = function (n) { return n < 10 ? '0' + n : n }
   if (typeof JSONPCallback === 'undefined') {
     var d = new Date()
@@ -21,15 +40,24 @@ Nikotama.prototype.setJSONPFunction = function (url, callback, JSONPCallback) {
     JSONPCallback += pad(d.getDate())
     JSONPCallback += this.JSONPCount
   }
+  // set global function
   window[JSONPCallback] = function (data) {
     try {
       callback(data)
     } catch (e) {}
+    // cleaning
     window[JSONPCallback] = undefined
   }
+  // add callback function to end of the url
   url += (url.indexOf('?') === -1) ? '?callback=' + JSONPCallback : '&callback=' + JSONPCallback
   return url
 }
+/**
+ * on
+ * @param target
+ * @param event
+ * @param callback
+ */
 Nikotama.prototype.on = function (target, event, callback) {
   if (document.addEventListener) {
     target.addEventListener(event, callback, false)
